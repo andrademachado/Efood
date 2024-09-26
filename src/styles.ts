a87f844e8600eb34081c8a856d6f67df9f77e7ac
Lígia Andrade <andradeligiamp@gmail.com>
2024-09-26 16:40:08 -0300

    tabela de cores

--- src/styles.ts
+++ src/styles.ts
@@ -1,10 +1,28 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from "styled-components"
 
-const GlobalCss = createGlobalStyle`
-    *{
+const cores = {   
+    vermelhoRosado:'#E66767',
+    branco: '#FFFFFF',
+    bege: '#FFEBD9'
+}
+
+export const GlobalCss = createGlobalStyle`
+    * {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
         font-family: Roboto, sans-serif;
     }
+
+    body{
+        background-color:  ${cores.bege};
+        color: ${cores.branco};
+    }
 `
+
+
+
+
+
+
+
