3be8c1289f1171b0ef74f27a04143311f7fd4f3b
Lígia Andrade <andradeligiamp@gmail.com>
2024-09-30 12:33:01 -0300

    Crieando variaveis das tabelas de cores

--- src/styles.ts
+++ src/styles.ts
@@ -1,11 +1,13 @@
 import { createGlobalStyle } from "styled-components"
 
+//tabela de cores  *(Variaveis pra usar em todo o projeto)
 const cores = {   
     vermelhoRosado:'#E66767',
     branco: '#FFFFFF',
     bege: '#FFEBD9'
 }
 
+//reset do estilo global
 export const GlobalCss = createGlobalStyle`
     * {
         margin: 0;
