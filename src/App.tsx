d0d32e77bf8af071b876c19511ddc6ec386076b9
Lígia Andrade <andradeligiamp@gmail.com>
2024-10-17 12:14:29 -0300

    Criando o arquivo routes e limpeza do App

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,6 @@
-import { createBrowserRouter, RouterProvider, BrowserRouter, Routes, Route } from 'react-router-dom'
+import {  BrowserRouter } from 'react-router-dom'
 import  {  GlobalCss } from './styles';
-import Home from './pages/Home';
-import Categories from './pages/Estabelecimento';
-import Estabelecimento from './pages/Estabelecimento';
-import Italia from './compnents/Italia';
-import Cartaz from './compnents/Cartaz';
-
-const Rotas = () => (
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/estabelecimento" element={<Estabelecimento />} />
-  </Routes>
-)
+import Rotas from './routes'
 
 function App() {
   return (
